3c7e7aca244ed8d92e53a316e0ea5bd235a0b7bf
d-yv <yuri.dubrovin@gmail.com>
2025-06-24 00:09:22 +0300

    fix error

--- char-string.js
+++ char-string.js
@@ -1,9 +1,8 @@
-In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 
-If anything in the text isn't a letter, ignore it and don't return it.
+// If anything in the text isn't a letter, ignore it and don't return it.
 
-"a" = 1, "b" = 2, etc.
-
+// "a" = 1, "b" = 2, etc.
 
 function stringChar(text) {
   const alphaBet = {};
